5c44e8030f60e1969e3989e9b98b4a442bea094a
laura <lauradondiego@gmail.com>
2019-08-14 20:07:47 -0400

    got my buyitem function to work

--- src/components/AddedFeature.js
+++ src/components/AddedFeature.js
@@ -1,13 +1,35 @@
-import React from 'react';
+import React from "react";
+import { connect } from "react-redux";
+
+import removeFeature from "../App";
 
 const AddedFeature = props => {
   return (
     <li>
       {/* Add an onClick to run a function to remove a feature */}
-      <button className="button">X</button>
+      <button
+        onClick={() => {
+          props.removeFeature(props.car.features);
+        }}
+        className="button"
+      >
+        X
+      </button>
       {props.feature.name}
     </li>
   );
 };
 
-export default AddedFeature;
+const mapStateToProps = state => {
+  console.log("state AF", state);
+  return {
+    additionalPrice: state.additionalPrice,
+    car: state.car,
+    store: state.store
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  { removeFeature }
+)(AddedFeature);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
 
-import 'bulma/css/bulma.css';
-import './styles.scss';
+import { carSalesReducer } from "./reducers/carSalesReducer";
 
-const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+import "bulma/css/bulma.css";
+import "./styles.scss";
+
+const store = createStore(carSalesReducer);
+
+console.log("store from index.js", store);
+
+const rootElement = document.getElementById("root");
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  rootElement
+);
